703a2fa68baec2ccf03a3442d2e5666460b7e6d6
Ahmed Tarek <ahmedtarekabd2002@gmail.com>
2025-04-22 13:21:42 +0200

    Add data insights, Apply MFCC on the first 500 samples, Apply optuna for hyperparameter tuning.

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+from tqdm import tqdm
+import pandas as pd
 
 DATA_DIR = Path("data")
+AUDIO_PATH = DATA_DIR / "audios"
 AUDIO_CACHE = DATA_DIR / "audio_cache"
 FEATURES_CACHE = DATA_DIR / "features_cache"
 MODELS_DIR = DATA_DIR / "models"
@@ -9,3 +12,5 @@ def run_config():
     AUDIO_CACHE.mkdir(parents=True, exist_ok=True)
     FEATURES_CACHE.mkdir(parents=True, exist_ok=True)
     MODELS_DIR.mkdir(parents=True, exist_ok=True)
+
+    tqdm.pandas()
