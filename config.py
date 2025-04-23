9f15855a7a5bcc72097ad92bfa5773a9a085cf96
Ahmed Tarek <ahmedtarekabd2002@gmail.com>
2025-04-23 19:33:12 +0200

    Update Feature Extraction: Add feature extraction methods other than mfccs, such as mfccs delta and delta 2, chroma, spectral_contrast, tonnetz, Energy & Zero Crossings Rate.

--- config.py
+++ config.py
@@ -1,16 +1,24 @@
+import os
 from pathlib import Path
 from tqdm import tqdm
 import pandas as pd
 
-DATA_DIR = Path("data")
+if os.path.exists("/kaggle"):
+    # Running on Kaggle
+    DATA_DIR = Path("/kaggle/input/your-dataset-name")
+elif os.path.exists("/content"):
+    # Running on Google Colab
+    DATA_DIR = Path("/content")
+else:
+    DATA_DIR = Path("data")
+
 AUDIO_PATH = DATA_DIR / "audios"
 AUDIO_CACHE = DATA_DIR / "audio_cache"
 FEATURES_CACHE = DATA_DIR / "features_cache"
 MODELS_DIR = DATA_DIR / "models"
 
 def run_config():
-    AUDIO_CACHE.mkdir(parents=True, exist_ok=True)
-    FEATURES_CACHE.mkdir(parents=True, exist_ok=True)
-    MODELS_DIR.mkdir(parents=True, exist_ok=True)
+    for folder in [AUDIO_CACHE, FEATURES_CACHE, MODELS_DIR]:
+        folder.mkdir(parents=True, exist_ok=True)
 
     tqdm.pandas()
